daa9c588098ebfb4d8869eef4a1f6ac936c83fa3
OrigamiGamer <3084943362@qq.com>
2024-08-27 15:53:19 +0800

    basic_channel

--- basic_chaos_engine/library/audio/basic_sound_player.cpp
+++ basic_chaos_engine/library/audio/basic_sound_player.cpp
@@ -5,6 +5,46 @@
 #ifdef _OPENAL
 namespace basic_chaos_engine {
     namespace basic_audio {
+        // basic_channel
+
+        void basic_channel::insert_sound(unsigned long long time_point, MAP_SOUND& map_sound) {
+            chronoqueue_sound[time_point] = map_sound;
+        }
+        void basic_channel::insert_sound(unsigned long long time_point, const std::wstring& sound_name, type::HANDLE_SOUND sound_handle) {}
+
+        bool basic_channel::remove(unsigned long long time_point) {
+            return chronoqueue_sound.erase(time_point) > 0;
+        }
+        bool basic_channel::remove(unsigned long long time_point, const std::wstring& sound_name) {
+            if (chronoqueue_sound.find(time_point) != chronoqueue_sound.end())
+                return chronoqueue_sound.at(time_point).erase(sound_name) > 0;
+            return false;
+        }
+        bool basic_channel::remove(unsigned long long time_point, type::HANDLE_SOUND sound_handle) {
+            if (chronoqueue_sound.find(time_point) != chronoqueue_sound.end()) {
+                auto _node_map_sound = chronoqueue_sound.at(time_point);
+                // foreach sound in map_sound at time_point
+                for (auto it = _node_map_sound.begin(); it != _node_map_sound.end(); it++)
+                    if (it->second == sound_handle) return _node_map_sound.erase(it->first) > 0;
+            }
+            return false;
+        }
+
+        bool basic_channel::clear() {
+            if (chronoqueue_sound.empty()) return false;
+            chronoqueue_sound.clear();
+            return true;
+        }
+
+        bool basic_channel::set_name(const std::wstring& name) {
+            if (name.empty()) return false;
+            channel_name = name;
+            return true;
+        }
+
+
+        // basic_sound_player
+
         basic_sound_player::basic_sound_player() {
         }
         bool basic_sound_player::initialize() {
@@ -23,7 +63,14 @@ namespace basic_chaos_engine {
             else return false;   // failed to create: soundname already exists
             return true;
         }
+        type::HANDLE_SOUND basic_sound_player::get_sound_handle(const std::wstring& soundname) {
+            if (map_sound.find(soundname) != map_sound.end())
+                return map_sound[soundname];
+            else return 0;  // failed to find sound: soundname does not exist
+        }
+        basic_channel basic_sound_player::create_channel(const std::wstring& channel_name) {
 
+        }
     }
 }
 #endif
--- basic_chaos_engine/library/audio/basic_sound_player.h
+++ basic_chaos_engine/library/audio/basic_sound_player.h
@@ -8,21 +8,49 @@
 
 namespace basic_chaos_engine {
     namespace basic_audio {
+        typedef std::map<const std::wstring, type::HANDLE_SOUND> MAP_SOUND;
+        typedef std::map<unsigned long long, MAP_SOUND> CHRONO_QUEUE_SOUND;
+
+        class basic_channel {
+        private:
+            std::wstring channel_name;
+            CHRONO_QUEUE_SOUND chronoqueue_sound;
+        public:
+            // Insert a map of sounds at a time point. Overwrite the existing sounds at this time point.
+            void insert_sound(unsigned long long time_point, MAP_SOUND& map_sound);
+            // TODO: Insert a sound into the map of sounds at a time point. Overwrite the existing sound with the same sound name at this time point.
+            void insert_sound(unsigned long long time_point, const std::wstring& sound_name, type::HANDLE_SOUND sound_handle);
+
+            // Remove all sounds at a time point.
+            bool remove(unsigned long long time_point);
+            // Remove a sound from a sound name at a time point.
+            bool remove(unsigned long long time_point, const std::wstring& sound_name);
+            // Remove all sounds with a same sound handle at a time point.
+            bool remove(unsigned long long time_point, type::HANDLE_SOUND sound_handle);
+
+            // Clear all sounds.
+            bool clear();
+
+            // Set the channel name.
+            bool set_name(const std::wstring& channel_name);
+        };
+
         class basic_sound_player {
         private:
 #ifdef _OPENAL
             basic_audio_openal openal;
 #endif
         public:
-            std::map<const std::wstring, type::HANDLE_SOUND> map_sound;
+            std::map<const std::wstring, type::HANDLE_SOUND> map_sound; // single sound resource
         public:
             basic_sound_player();
             bool initialize();
             bool release();
             // Load and create a sound from a file. 'soundname' is optional and can be used to get the sound resource later.
             bool load_sound_file(const std::wstring& filename, const std::wstring& soundname = {});
-            bool create_channel();
-            bool create_channel_group();
+            type::HANDLE_SOUND get_sound_handle(const std::wstring& soundname);
+            basic_channel create_channel(const std::wstring& channel_name);
+            bool create_channel_group(const std::wstring& channel_group_name);
             bool playback();
         };
     }
