c09f8f63541a5a39ffae07da95015f5ad2657805
Origami <3084943362@qq.com>
2024-11-17 01:52:37 +0800

    added volume control api of basic_audio_openal

--- basic_chaos_engine/libraries/audio/OpenAL/basic_audio_openal.cpp
+++ basic_chaos_engine/libraries/audio/OpenAL/basic_audio_openal.cpp
@@ -2,34 +2,41 @@
 
 #include "basic_audio_openal.h"
 
-namespace basic_chaos_engine {
-    basic_audio_openal::basic_audio_openal() {
+namespace basic_chaos_engine
+{
+    basic_audio_openal::basic_audio_openal()
+    {
         _device = nullptr;
         _context = nullptr;
     }
-    inline int basic_audio_openal::get_error() {
+    inline int basic_audio_openal::get_error()
+    {
         return (_last_error != AL_NO_ERROR) ? _last_error : alGetError();
     }
-    bool basic_audio_openal::initialize() {
+    bool basic_audio_openal::initialize()
+    {
         // create device
         _device = alcOpenDevice(0);
         if (!_device) return false;
         // create context
         _context = alcCreateContext(_device, 0);
-        if (!_context) {
+        if (!_context)
+        {
             alcCloseDevice(_device);
             return false;
         }
         alcMakeContextCurrent(_context);
         return true;
     }
-    bool basic_audio_openal::release() {
+    bool basic_audio_openal::release()
+    {
         for (auto& _source_id : vec_source_id) alDeleteSources(1, &_source_id);
         for (auto& _buffer_id : vec_buffer_id) alDeleteBuffers(1, &_buffer_id);
         alcDestroyContext(_context);
         return alcCloseDevice(_device);
     }
-    type::BufferID basic_audio_openal::load_sound_file(const std::wstring& filename) {
+    type::BufferID basic_audio_openal::load_sound_file(const std::wstring& filename)
+    {
         SF_INFO info{};
         SNDFILE* file = sf_wchar_open(program::locate(filename).c_str(), SFM_READ, &info);
         if (file == nullptr) return 0; // failed to open file
@@ -47,80 +54,108 @@ namespace basic_chaos_engine {
         vec_buffer_id.push_back(bufferID);
         return bufferID;
     }
-    type::SourceID basic_audio_openal::create_source() {
+    type::SourceID basic_audio_openal::create_source()
+    {
         type::SourceID sourceID{};
         alGenSources(1, &sourceID);
         vec_source_id.push_back(sourceID);
         return sourceID;
     }
-    inline bool basic_audio_openal::source_queue_buffer(type::SourceID source_id, type::BufferID buffer_id) {
+    inline bool basic_audio_openal::source_queue_buffer(type::SourceID source_id, type::BufferID buffer_id)
+    {
         alSourceQueueBuffers(source_id, 1, &buffer_id);
         if ((_last_error = alGetError()) != AL_NO_ERROR) return false;
         return true;
     }
-    inline bool basic_audio_openal::source_unqueue_buffer(type::SourceID source_id, type::BufferID buffer_id) {
+    inline bool basic_audio_openal::source_unqueue_buffer(type::SourceID source_id, type::BufferID buffer_id)
+    {
         alSourceUnqueueBuffers(source_id, 1, &buffer_id);
         if ((_last_error = alGetError()) != AL_NO_ERROR) return false;
         return true;
     }
-    inline bool basic_audio_openal::source_play(type::SourceID source_id) {
+    inline bool basic_audio_openal::source_play(type::SourceID source_id)
+    {
         alSourcePlay(source_id);
         if ((_last_error = alGetError()) != AL_NO_ERROR) return false;
         return true;
     }
-    inline bool basic_audio_openal::source_pause(type::SourceID source_id) {
+    inline bool basic_audio_openal::source_pause(type::SourceID source_id)
+    {
         alSourcePause(source_id);
         if ((_last_error = alGetError()) != AL_NO_ERROR) return false;
         return true;
     }
-    inline bool basic_audio_openal::source_stop(type::SourceID source_id) {
+    inline bool basic_audio_openal::source_stop(type::SourceID source_id)
+    {
         alSourceStop(source_id);
         if ((_last_error = alGetError()) != AL_NO_ERROR) return false;
         return true;
     }
-    inline bool basic_audio_openal::set_general_speed(ALfloat speed) {
+    inline bool basic_audio_openal::set_general_speed(ALfloat speed)
+    {
         alSpeedOfSound(speed);
         if ((_last_error = alGetError()) != AL_NO_ERROR) return false;
         return true;
     }
-    inline bool basic_audio_openal::set_general_pitch(ALfloat pitch) {
+    inline bool basic_audio_openal::set_general_pitch(ALfloat pitch)
+    {
         alDopplerFactor(pitch);
         if (alGetError() != AL_NO_ERROR) return false;
         return true;
     }
-    inline bool basic_audio_openal::source_rewind(type::SourceID source_id) {
+    inline bool basic_audio_openal::source_rewind(type::SourceID source_id)
+    {
         alSourceRewind(source_id);
         if ((_last_error = alGetError()) != AL_NO_ERROR) return false;
         return true;
     }
-    inline int basic_audio_openal::source_get_playback_position(type::SourceID source_id) {
+    inline int basic_audio_openal::source_get_playback_position(type::SourceID source_id)
+    {
         int _pos = 0;
         alGetSourcei(source_id, AL_SAMPLE_OFFSET, &_pos);
         if ((_last_error = alGetError()) != AL_NO_ERROR) return 0;
         return _pos;
     }
-    inline int basic_audio_openal::source_get_playback_time(type::SourceID source_id) {
+    inline int basic_audio_openal::source_get_playback_time(type::SourceID source_id)
+    {
         float _time = 0;
         alGetSourcef(source_id, AL_SEC_OFFSET, &_time);
         if ((_last_error = alGetError()) != AL_NO_ERROR) return 0;
         return static_cast<int>(_time * 1000.f);
     }
-    inline bool basic_audio_openal::source_set_playback_position(type::SourceID source_id, int position) {
+    inline float basic_audio_openal::source_get_volume(type::SourceID source_id)
+    {
+        float _volume = 0;
+        alGetSourcef(source_id, AL_GAIN, &_volume);
+        if ((_last_error = alGetError()) != AL_NO_ERROR) return 0;
+        return _volume;
+    }
+    inline bool basic_audio_openal::source_set_playback_position(type::SourceID source_id, int position)
+    {
         alSourcei(source_id, AL_SAMPLE_OFFSET, position);
-        if ((_last_error = alGetError()) != AL_NO_ERROR) return 0;
+        if ((_last_error = alGetError()) != AL_NO_ERROR) return false;
         return true;
     }
-    inline bool basic_audio_openal::source_set_playback_time(type::SourceID source_id, int time) {
+    inline bool basic_audio_openal::source_set_playback_time(type::SourceID source_id, int time)
+    {
         alSourcef(source_id, AL_SEC_OFFSET, static_cast<float>(time) / 1000.f);
-        if ((_last_error = alGetError()) != AL_NO_ERROR) return 0;
+        if ((_last_error = alGetError()) != AL_NO_ERROR) return false;
         return true;
     }
-    type::SoundData basic_audio_openal::source_get_sound_data(type::SourceID source_id, int position, int size) {
+    type::SoundData basic_audio_openal::source_get_sound_data(type::SourceID source_id, int position, int size)
+    {
         ALint sample_offset = this->source_get_playback_position(source_id);
         // type::SoundData data(size*);
         return {};
     }
-    void basic_audio_openal::buffer_get_sound_data(type::BufferID buffer_id, int position, int size) {
+    inline bool basic_audio_openal::source_set_volume(type::SourceID source_id, float volume)
+    {
+        alSourcef(source_id, AL_GAIN, volume);
+        if ((_last_error = alGetError()) != AL_NO_ERROR) return false;
+        return true;
+    }
+    void basic_audio_openal::buffer_get_sound_data(type::BufferID buffer_id, int position, int size)
+    {
 
     }
 }
--- basic_chaos_engine/libraries/audio/OpenAL/basic_audio_openal.h
+++ basic_chaos_engine/libraries/audio/OpenAL/basic_audio_openal.h
@@ -2,8 +2,10 @@
 
 #include "common_audio.h"
 
-namespace basic_chaos_engine {
-    class basic_audio_openal {
+namespace basic_chaos_engine
+{
+    class basic_audio_openal
+    {
     private:
         ALenum _last_error;
         ALCdevice* _device;
@@ -27,21 +29,35 @@ namespace basic_chaos_engine {
         void buffer_get_sound_data(type::BufferID buffer_id, int position, int size);
 
         type::SourceID create_source();
+
         // Queue a buffer to a source. Return false if the ID of source or buffer is invalid.
         inline bool source_queue_buffer(type::SourceID source_id, type::BufferID buffer_id);
+
         // Unqueue a buffer from a source. Return false if the ID of source or buffer is invalid.
         inline bool source_unqueue_buffer(type::SourceID source_id, type::BufferID buffer_id);
+
         // Playback a source. Return false if the ID of source is invalid.
         inline bool source_play(type::SourceID source_id);
         inline bool source_pause(type::SourceID source_id);
         inline bool source_stop(type::SourceID source_id);
         inline bool source_rewind(type::SourceID source_id);
+
+        inline int source_get_volume(type::SourceID source_id, float volume);
+
         inline int source_get_playback_position(type::SourceID source_id);
+
         // Units: milliseconds(ms).
         inline int source_get_playback_time(type::SourceID source_id);
+
+        inline float source_get_volume(type::SourceID source_id);
+
         inline bool source_set_playback_position(type::SourceID source_id, int position);
+
         // Units: milliseconds(ms).
         inline bool source_set_playback_time(type::SourceID source_id, int time);
+
+        inline bool source_set_volume(type::SourceID source_id, float volume);
+
         // TODO: connect TTFW, libsndfile and OpenAL.
         type::SoundData source_get_sound_data(type::SourceID source_id, int position, int size);
     };
